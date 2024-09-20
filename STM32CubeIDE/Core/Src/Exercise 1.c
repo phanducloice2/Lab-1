c7ec7b62aa62a89039a7e2b4fb6c4d409c47979e
phanducloi08012004viettel <phanducloi08012004viettel@gmail.com>
2024-09-20 09:21:09 +0700

    1

--- STM32CubeIDE/Core/Src/Exercise 1.c
+++ STM32CubeIDE/Core/Src/Exercise 1.c
@@ -5,5 +5,19 @@
  *      Author: Phan Duc Loi
  */
 #include "Exercise 1.h"
+int count=2;
+int status=1;
 void init_exercise1(){}
-void run_exercise1(){}
+void led_red(){
+	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, SET);
+	HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, RESET);
+}
+void led_yellow(){
+	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, RESET);
+	HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, SET);
+}
+void run_exercise1(){
+	if (count!=0&&status==0) {led_red(); count--;}
+	if (count!=0&&status==1) {led_yellow();count--;}
+	if (count==0) {status=!status; count=2;}
+}
