b27e262b93b65fefb403e6ba85b1a77053c88e8b
phanducloi08012004viettel <phanducloi08012004viettel@gmail.com>
2024-09-20 10:00:31 +0700

    3

--- STM32CubeIDE/Core/Src/Exercise 3.c
+++ STM32CubeIDE/Core/Src/Exercise 3.c
@@ -5,11 +5,45 @@
  *      Author: Phan Duc Loi
  */
 #include "Exercise 3.h"
-int count=5;
-int status=0;
+int count=10;
 void init_exercise3(){}
-void led
-void run_exercise3(){}
+void led_red1(){
+	HAL_GPIO_WritePin(red1_GPIO_Port, red1_Pin, RESET);
+	HAL_GPIO_WritePin(yellow1_GPIO_Port, yellow1_Pin, SET);
+	HAL_GPIO_WritePin(green1_GPIO_Port, green1_Pin, SET);
+}
+void led_yellow1(){
+	HAL_GPIO_WritePin(red1_GPIO_Port, red1_Pin, SET);
+	HAL_GPIO_WritePin(yellow1_GPIO_Port, yellow1_Pin, RESET);
+	HAL_GPIO_WritePin(green1_GPIO_Port, green1_Pin, SET);
+}
+void led_green1(){
+	HAL_GPIO_WritePin(red1_GPIO_Port, red1_Pin, SET);
+	HAL_GPIO_WritePin(yellow1_GPIO_Port, yellow1_Pin, SET);
+	HAL_GPIO_WritePin(green1_GPIO_Port, green1_Pin, RESET);
+}
+void led_red2(){
+	HAL_GPIO_WritePin(red2_GPIO_Port, red2_Pin, RESET);
+	HAL_GPIO_WritePin(yellow2_GPIO_Port, yellow2_Pin, SET);
+	HAL_GPIO_WritePin(green2_GPIO_Port, green2_Pin, SET);
+}
+void led_yellow2(){
+	HAL_GPIO_WritePin(red2_GPIO_Port, red2_Pin, SET);
+	HAL_GPIO_WritePin(yellow2_GPIO_Port, yellow2_Pin, RESET);
+	HAL_GPIO_WritePin(green2_GPIO_Port, green2_Pin, SET);
+}
+void led_green2(){
+	HAL_GPIO_WritePin(red2_GPIO_Port, red2_Pin, SET);
+	HAL_GPIO_WritePin(yellow2_GPIO_Port, yellow2_Pin, SET);
+	HAL_GPIO_WritePin(green2_GPIO_Port, green2_Pin, RESET);
+}
+void run_exercise3(){
+	if (count>7){led_red1();led_green2();count--;}
+	else if (count>5){led_red1();led_yellow2();count--;}
+	else if (count>2){led_green1();led_red2();count--;}
+	else if (count>0){led_yellow1();led_red2();count--;}
+	else count=10;
+}
 
 
 
