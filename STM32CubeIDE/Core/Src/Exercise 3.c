28647cba69c646156eb436b593c49ff09da434a0
phanducloi08012004viettel <phanducloi08012004viettel@gmail.com>
2024-09-20 10:17:53 +0700

    3

--- STM32CubeIDE/Core/Src/Exercise 3.c
+++ STM32CubeIDE/Core/Src/Exercise 3.c
@@ -8,34 +8,34 @@
 int count=10;
 void init_exercise3(){}
 void led_red1(){
-	HAL_GPIO_WritePin(red1_GPIO_Port, red1_Pin, RESET);
-	HAL_GPIO_WritePin(yellow1_GPIO_Port, yellow1_Pin, SET);
-	HAL_GPIO_WritePin(green1_GPIO_Port, green1_Pin, SET);
-}
-void led_yellow1(){
 	HAL_GPIO_WritePin(red1_GPIO_Port, red1_Pin, SET);
 	HAL_GPIO_WritePin(yellow1_GPIO_Port, yellow1_Pin, RESET);
-	HAL_GPIO_WritePin(green1_GPIO_Port, green1_Pin, SET);
+	HAL_GPIO_WritePin(green1_GPIO_Port, green1_Pin, RESET);
 }
-void led_green1(){
-	HAL_GPIO_WritePin(red1_GPIO_Port, red1_Pin, SET);
+void led_yellow1(){
+	HAL_GPIO_WritePin(red1_GPIO_Port, red1_Pin, RESET);
 	HAL_GPIO_WritePin(yellow1_GPIO_Port, yellow1_Pin, SET);
 	HAL_GPIO_WritePin(green1_GPIO_Port, green1_Pin, RESET);
 }
-void led_red2(){
-	HAL_GPIO_WritePin(red2_GPIO_Port, red2_Pin, RESET);
-	HAL_GPIO_WritePin(yellow2_GPIO_Port, yellow2_Pin, SET);
-	HAL_GPIO_WritePin(green2_GPIO_Port, green2_Pin, SET);
+void led_green1(){
+	HAL_GPIO_WritePin(red1_GPIO_Port, red1_Pin, RESET);
+	HAL_GPIO_WritePin(yellow1_GPIO_Port, yellow1_Pin, RESET);
+	HAL_GPIO_WritePin(green1_GPIO_Port, green1_Pin, SET);
 }
-void led_yellow2(){
+void led_red2(){
 	HAL_GPIO_WritePin(red2_GPIO_Port, red2_Pin, SET);
 	HAL_GPIO_WritePin(yellow2_GPIO_Port, yellow2_Pin, RESET);
-	HAL_GPIO_WritePin(green2_GPIO_Port, green2_Pin, SET);
+	HAL_GPIO_WritePin(green2_GPIO_Port, green2_Pin, RESET);
 }
-void led_green2(){
-	HAL_GPIO_WritePin(red2_GPIO_Port, red2_Pin, SET);
+void led_yellow2(){
+	HAL_GPIO_WritePin(red2_GPIO_Port, red2_Pin, RESET);
 	HAL_GPIO_WritePin(yellow2_GPIO_Port, yellow2_Pin, SET);
 	HAL_GPIO_WritePin(green2_GPIO_Port, green2_Pin, RESET);
+}
+void led_green2(){
+	HAL_GPIO_WritePin(red2_GPIO_Port, red2_Pin, RESET);
+	HAL_GPIO_WritePin(yellow2_GPIO_Port, yellow2_Pin, RESET);
+	HAL_GPIO_WritePin(green2_GPIO_Port, green2_Pin, SET);
 }
 void run_exercise3(){
 	if (count>7){led_red1();led_green2();count--;}
