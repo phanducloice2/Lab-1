6da68645c2e67acc44d958d32da891ec879bf136
phanducloi08012004viettel <phanducloi08012004viettel@gmail.com>
2024-09-20 08:38:19 +0700

    3

--- STM32CubeIDE/Core/Src/main.c
+++ STM32CubeIDE/Core/Src/main.c
@@ -64,7 +64,7 @@ static void MX_GPIO_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-  init_exercise1();
+	init_exercise1();
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -93,7 +93,7 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
+	  run_exercise1();
 	  HAL_Delay(1000);
     /* USER CODE END WHILE */
 
